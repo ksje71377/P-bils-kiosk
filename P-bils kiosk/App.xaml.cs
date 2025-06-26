f190b6b5a040d573037be14992e381f63a792414
ksje713777 <ksje71377@edu.ucl.dk>
2025-06-26 17:21:34 +0200

    Add project files.

---
+++ P-bils kiosk/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P_bils_kiosk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
