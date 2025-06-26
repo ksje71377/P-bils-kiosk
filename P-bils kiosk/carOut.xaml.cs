843a87d1734423fe8c10e53b95c4943c3d79679a
ksje713777 <ksje71377@edu.ucl.dk>
2025-06-26 21:05:54 +0200

    Tilføjede vindue til OUT

---
+++ P-bils kiosk/carOut.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P_bils_kiosk
+{
+    /// <summary>
+    /// Interaction logic for carOut.xaml
+    /// </summary>
+    public partial class carOut : Window
+    {
+        public carOut()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
