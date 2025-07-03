197d431f6fc8daf823d5a7161d9f8e3f086d5f05
ksje713777 <ksje71377@edu.ucl.dk>
2025-07-03 23:06:50 +0200

    Flyttet filer ud i respektive mapper aht MVVM struktur
    Installeret ClosedXML for Excel support
    
    Igangværende export fil (WIP) - ikke funktionel

---
+++ P-bils kiosk/Helpers/ExcelExporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Math;
+using DocumentFormat.OpenXml.Vml.Office;
+using P_bils_kiosk.Models;
+
+namespace P_bils_kiosk.Helpers
+{
+    class ExcelExporter
+    {
+        string dato = entry.Tidspunkt.ToString("dd-MM-yyyy");
+        string filnavn = $"{dato}.xlsx";
+        bool filEksisterer = File.Exists(filnavn);
+    }
+}
---
+++ P-bils kiosk/Models/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_bils_kiosk.Models
+{
+        public class CarLogEntry
+        {
+            public string chaufførNummer { get; set; }
+            public string valgtBil { get; set; }
+            public string destination { get; set; }
+            public DateTime Tidspunkt { get; set; } = DateTime.Now;
+           public bool isOutbound { get; set; } = true; // true for udgående, false for indgående
+    }
+    }
